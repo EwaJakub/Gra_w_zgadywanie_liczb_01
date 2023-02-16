f0fea4bd2717592d638a6c09dc13d33842c98ef9
EwaJakub <ewa.jak@onet.pl>
2023-02-16 19:28:36 +0100

    final_gra_w_zgasywanie_liczb_01

--- Gra_w_zgadywanie_liczb.py
+++ Gra_w_zgadywanie_liczb.py
@@ -1,26 +1,36 @@
-import random
+from random import randint
+
 
 def guessed_number():
+    """
+    Getting number from user until receiving guessed number.
+    :return: number as int.
+    """
     while True:
         try:
-            guessed_number = int(input("Guess the number: "))
-            return guessed_number
+            return int(input("Guess the number: "))
         except ValueError:
             print("It's not a number!")
 
-chosen_number = random.choice(range(1, 101))
 
 def guess_the_number():
-    number_too_choose = guessed_number()
-    if number_too_choose < chosen_number:
-        print("Too small!")
-        guess_the_number()
-    elif number_too_choose > chosen_number:
-        print("To big!")
-        guess_the_number()
-    elif number_too_choose == chosen_number:
-        print("You Win!")
+    """
+    Guessing the right number, same as chosen number on base of hints
+    based on the value given from guessed_number function.
+    :return:
+    """
+    chosen_number = randint(1, 100)
+    guessed = False
+    while not guessed:
+        number_too_choose = guessed_number()
+        if number_too_choose < chosen_number:
+            print("Too small!")
+        elif number_too_choose > chosen_number:
+            print("To big!")
+        elif number_too_choose == chosen_number:
+            guessed = True
+            print("you win!")
 
 
 if __name__ == "__main__":
- guess_the_number()
+    guess_the_number()
